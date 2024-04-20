739ea03628cfdf511ebb38906317efdcf735cab7
alexvnguyen24 <108242054+alexvnguyen24@users.noreply.github.com>
2024-04-20 03:56:52 -0400

    Help button additions
    added prototype for hint button

---
+++ src/client/script.js
@@ -0,0 +1,8 @@
+const tutorialButton = document.getElementById("tutorial_button");
+tutorialButton.addEventListener("click", function(){
+    /*const textBox = document.createElement("div");
+    textBox.innerText = "test";
+
+    const instructions = document.getElementById("tutorial-text");
+    instructions.appendChild(textBox);*/
+});
