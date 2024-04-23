b0bc8254a0f62d9b0d322f722f6c33ab052ae784
Phounthasenlam <133175100+Phounthasenlam@users.noreply.github.com>
2024-04-23 19:56:35 -0400

    yuh - Rickys Part
    What I did ( I hope it doesnt break )

---
+++ src/client/script.js
@@ -0,0 +1,8 @@
+
+
+
+const playbuttonElement = document.getElementById("play_button");
+
+
+playbuttonElement.addEventListener("click", () => {document.body.style.backgroundColor = 'black'; window.location.href = "menu.html"});
+
