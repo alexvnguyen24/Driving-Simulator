07f4f0e742b11ca548b3109c0a98c8c258dc214d
alexvnguyen24 <108242054+alexvnguyen24@users.noreply.github.com>
2024-04-22 21:32:52 -0400

    Added more button details

--- src/client/script.js
+++ src/client/script.js
@@ -1,8 +1,24 @@
 const tutorialButton = document.getElementById("tutorial_button");
 tutorialButton.addEventListener("click", function(){
-    /*const textBox = document.createElement("div");
-    textBox.innerText = "test";
+    if(!document.getElementById("tutorial-box")){
+        const textBox = document.createElement("div");
+        const h1 = document.createElement("h1");
+        const p = document.createElement("p");
 
-    const instructions = document.getElementById("tutorial-text");
-    instructions.appendChild(textBox);*/
+        h1.innerHTML = "How To Play";
+        p.innerHTML = "instructions";
+        textBox.setAttribute("id", "tutorial-box");
+        p.setAttribute("class", "instruction-paragraph");
+
+        const instructions = document.getElementById("tutorial-text");
+        instructions.appendChild(textBox);
+        textBox.appendChild(h1);
+        textBox.appendChild(p);
+    }
+    else{
+        const removeElement = document.getElementById("tutorial-text");
+        while(removeElement.firstChild){
+            removeElement.removeChild(removeElement.firstChild);
+        }
+    }
 });
